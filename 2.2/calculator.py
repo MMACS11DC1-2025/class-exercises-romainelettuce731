0f8516f28edac15e1d9fe169a8a09a6a78087724
romainelettuce731 <1536083@learn.vsb.bc.ca>
2025-09-22 14:07:49 -0700

    Update calculator.py

--- 2.2/calculator.py
+++ 2.2/calculator.py
@@ -3,8 +3,16 @@ Machines are good at crunching numbers - faster and more accurately than most
 humans! Create a small program that calculates something useful to you 
 (making you smile is useful). It should take user input, at use at least one of the 
 number operators we saw in class: + / * . You may modify one of your previous 
-exercises to include calculations, if you wish.
-
-Remember to design your algorithm in English first, then translate it to Python 
+exercises to include calculations, if you wish. Remember to design your 
+algorithm in English first, then translate it to Python 
 code. Test as you go!
 """
+
+#calculator
+#mason lui
+#sept 22
+
+print("helo welom to banan store one banan is 79 cents yum yunm give me money")
+banan = int(input("how many banana?))
+
+print(banan*79)
