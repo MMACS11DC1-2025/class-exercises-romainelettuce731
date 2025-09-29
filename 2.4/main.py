dfc5580f28506e164e1364fe7a588dc6cf0987e6
romainelettuce731 <1536083@learn.vsb.bc.ca>
2025-09-29 21:19:08 +0000

    iejdie

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,5 +1,15 @@
 file = open("2.4/responses.csv")
-for i in file:
+count = 0
+cat = 0
+for line in file:
+    count+=1
     if "mason" in line.lower():
         print(line)
+        print("Mason is found on line "+str(count))
+        
+    if "cat" in line.lower():
+        cat+=1
+
+print(str(cat)+ " people like cats.")
+
     
