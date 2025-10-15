2225f72ad563bcb5bb92f59db9c5c94827f2451c
romainelettuce731 <1536083@learn.vsb.bc.ca>
2025-10-15 22:02:56 +0000

    ssss

--- 4.1/drawing.py
+++ 4.1/drawing.py
@@ -4,3 +4,24 @@ Explore the turtle drawing package to create an interactive drawing.
 It should use a while loop.
 Your program should also include at least one function you’ve made yourself 
 """
+import turtle
+turty = turtle.Turtle()
+
+turty.speed(0)
+size = input("How big do you want your smiley saw to be?")
+size = int(size)
+count = 0
+while True:
+    def spike(x,y):
+      for i in range(100):
+        turty.forward(10 + size)
+        turty.right(90 + size)
+        turty.forward(10 + size)
+    spike(1, 1)
+    turty.penup()
+    turty.forward(50 + size)
+    turty.pendown()
+    count += 1
+    
+    if count == 6:
+      break
