4d799aa7f79a1ce3430c195b56cff2819a217ae3
romainelettuce731 <1536083@learn.vsb.bc.ca>
2025-11-17 21:32:19 +0000

    im so tired

--- 5.1/binarzised_image.py
+++ 5.1/binarzised_image.py
@@ -1,4 +1,20 @@
 from PIL import Image
 import is_binarized
-image_pear = Image.open("5.1./pearto.jpeg").load()
-is_light(image_pear)
+
+image_pear = Image.open("5.1/pearto.jpeg").load()
+
+image_output = Image.open('5.1/pearto.jpeg')
+width = image_output.width
+height = image_output.height
+
+for i in range(width):
+    for j in range(height):
+        im_r = image_pear[i, j][0]
+        im_g = image_pear[i, j][1]
+        im_b = image_pear[i, j][2]
+        if is_binarized.is_light((im_r, im_g, im_b)) == True:
+            image_output.putpixel((i, j), (255, 255, 255))
+        else:
+            image_output.putpixel((i, j), (0, 0, 0))
+
+image_output.save('output2.png', 'png')
--- 5.1/is_binarized.py
+++ 5.1/is_binarized.py
@@ -1,5 +1,5 @@
 def is_light(pixel):
-    if pixel[0] + pixel[1] + pixel[2]/3 > 128:
+    if int(pixel[0] + pixel[1] + pixel[2])/3 > 128:
         return True
     else:
         return False
