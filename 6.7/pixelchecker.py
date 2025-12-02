b44d99edf65f9fc451bea18427b618a7f719dd98
romainelettuce731 <1536083@learn.vsb.bc.ca>
2025-12-02 21:40:12 +0000

    optimized the code so it doesn't take 9 mins :D

--- 6.7/pixelchecker.py
+++ 6.7/pixelchecker.py
@@ -3,40 +3,49 @@ import time
 
 t0 = time.time()
 
-colorlist = []
-
-def is_pixel(r, g, b):
-    rgb = (r, g, b)
-    if rgb not in colorlist:
-        colorlist.append(rgb)
-        return True
-    else:
-        return False
+uniquecolors = []
+pixelart = []
+pixelartlen = []
+brushart = []
+brushartlen = []
 
 def pixel_art_check(imgname):
     inp_img = Image.open(imgname)
-    width = inp_img.width
-    height = inp_img.height
-    currentimg = Image.open(imgname).load()
-    for i in range(width):
-        for j in range(height):
-            im_r = currentimg[i, j][0]
-            im_g = currentimg[i, j][1]
-            im_b = currentimg[i, j][2]
-            is_pixel(im_r, im_g, im_b)
-    inp_img.save("outputtest.png", "png")
-    print(len(colorlist))
+    pixels = inp_img.getdata()
+    uniquecolors = set(pixels)
+    print(len(uniquecolors))
 
+def list_append(imgname, pixcount):
+    if len(uniquecolors) <= 114500:
+        pixelart.append(imgname)
+        pixelartlen.append(pixcount)
+    else:
+        brushart.append(imgname)
+        brushartlen.append(pixcount)
+
+#------------------------------------------------------------------------
 t1 = time.time()
 
 pixel_art_check('6.7/Deltarune-poster.png')
+list_append('6.7/Deltarune-poster.png', len(uniquecolors))
+uniquecolors = []
 t2 = time.time()
 
 pixel_art_check('6.7/Undertale_Cast.png')
+list_append('6.7/Undertale_Cast.png', len(uniquecolors))
+uniquecolors = []
 t3 = time.time()
 
-#pixel_art_check('6.7/World_1-1_Super_Mario_Bros.png')
-
 pixel_art_check('6.7/raiseupyourbat.jpg')
+list_append('6.7/raiseupyourbat.jpg', len(uniquecolors))
 t4 = time.time()
-print('{:.2f} {:.2f} {:.2f} {:.2f} {:.2f}'.format(t1-t0, t2-t1, t3-t2, t4-t3, t4-t0))
+
+pixel_art_check('6.7/hatsune-miku.jpg')
+list_append('6.7/hatsune-miku.jpg', len(uniquecolors))
+t4 = time.time()
+
+print(pixelart)
+print(brushart)
+print(pixelartlen)
+print(brushartlen)
+print('Loading the functions and modules took {:.2f} seconds. The first image took {:.2f} seconds, the second image took {:.2f} seconds, the third image took {:.2f} seconds, and in total it took {:.2f} seconds.'.format(t1-t0, t2-t1, t3-t2, t4-t3, t4-t0))
