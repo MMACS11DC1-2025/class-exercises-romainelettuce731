ce6cfcce4d1519a84527ad53cae665e9f7d2b5e9
romainelettuce731 <1536083@learn.vsb.bc.ca>
2025-12-03 21:34:36 +0000

    edeed

--- 6.2/search.py
+++ 6.2/search.py
@@ -1,6 +1,6 @@
 file = open("6.2/spotify.csv")
 junk = file.readline()
-
+count = 0
 drake_data = []
 
 for line in file:
@@ -12,6 +12,16 @@ for line in file:
 	if artist == "Drake":
 		drake_data.append([danceability, songtitle, artist])
 
+for i in range(len(drake_data)):
+	largest_score = drake_data[i]
+	largest_index = i
+	for j in range(i+1, len(drake_data)):
+		if drake_data[j] > largest_score:
+			largest_score = drake_data[j]
+			largest_index = j
+
+	drake_data[largest_index], drake_data[i] = drake_data[i], drake_data[largest_index]
+print("The top 5 dancable Drake songs are:")
 print("Dance score \tSong")
-for item in drake_data:
+for item in drake_data[:5]:
 	print(str(item[0]) + "\t\t" + item[1] + " by " + item[2])
--- 6.7/pixelchecker.py
+++ 6.7/pixelchecker.py
@@ -42,10 +42,15 @@ t4 = time.time()
 
 pixel_art_check('6.7/hatsune-miku.jpg')
 list_append('6.7/hatsune-miku.jpg', len(uniquecolors))
-t4 = time.time()
+t5 = time.time()
+
+pixel_art_check('6.7/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg')
+list_append('6.7/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg', len(uniquecolors))
+t6 = time.time()
 
 print(pixelart)
 print(brushart)
 print(pixelartlen)
 print(brushartlen)
-print('Loading the functions and modules took {:.2f} seconds. The first image took {:.2f} seconds, the second image took {:.2f} seconds, the third image took {:.2f} seconds, and in total it took {:.2f} seconds.'.format(t1-t0, t2-t1, t3-t2, t4-t3, t4-t0))
+print('Loading the functions and modules took {:.2f} seconds. The first image took {:.2f} seconds, the second image took {:.2f} seconds, the third image took {:.2f} seconds,'.format(t1-t0, t2-t1, t3-t2, t4-t3,))
+print('the fourth image took {:.2f} seconds, the fifth image took {:.2f} seconds'.format(t5-t4, t6-t5))
